1ec55e729ea697e7e09506f6bb7de14019c543b3
bradsloan <email": null>
2017-08-31 17:37:00 -0400

    compress function

--- convert_table.rb
+++ convert_table.rb
@@ -1,9 +1,12 @@
 
 
 def convert_table(table)
-  converted_table = []
-  converted_table << {table[0][0] => table[1][0], table[0][1] => table[1][1], table[0][2] => table[1][2], table[0][3] => table[1][3]}
-  converted_table << {table[0][0] => table[2][0], table[0][1] => table[2][1], table[0][2] => table[2][2], table[0][3] => table[2][3]}
-  converted_table << {table[0][0] => table[3][0], table[0][1] => table[3][1], table[0][2] => table[3][2], table[0][3] => table[3][3]}
-  converted_table
+  # converted_table = []
+  # converted_table << {table[0][0] => table[1][0], table[0][1] => table[1][1], table[0][2] => table[1][2], table[0][3] => table[1][3]}
+  # converted_table << {table[0][0] => table[2][0], table[0][1] => table[2][1], table[0][2] => table[2][2], table[0][3] => table[2][3]}
+  # converted_table << {table[0][0] => table[3][0], table[0][1] => table[3][1], table[0][2] => table[3][2], table[0][3] => table[3][3]}
+  # converted_table
+
+  table[1..-1].map{|row| Hash[table[0].zip(row)]}
+
 end
