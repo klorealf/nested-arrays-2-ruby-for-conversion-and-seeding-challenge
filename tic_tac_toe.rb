21c35000bb56734ce41e1e812e17044b92025938
David Skaggs <davidtskaggs@gmail.com>
2017-08-31 18:48:41 -0400

    Refactor method

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -1,6 +1,4 @@
 def generate_tic_tac_toe
-  game_move = ["X", "O"]
-  game_board = Array.new(3, game_move.sample) + Array.new(3, game_move.sample) + Array.new(3, game_move.sample)
-  p game_board
+game_moves = ["X", "O"]
+game_board = Array.new(3, game_moves.sample) {Array.new(3) {game_moves.sample}}
 end
-generate_tic_tac_toe
