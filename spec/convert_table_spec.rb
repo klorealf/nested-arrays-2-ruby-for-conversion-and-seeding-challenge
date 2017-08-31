1f331c61ca2ebe3e98946c43e40d8e08f2d975dd
bradsloan <email": null>
2017-08-31 11:11:26 -0400

    create array to hash function for 4 by 4 array

--- spec/convert_table_spec.rb
+++ spec/convert_table_spec.rb
@@ -1,4 +1,21 @@
 require_relative '../convert_table'
 
 describe 'converting a roster from a nested array to a collection of hashes' do
+
+  it 'outputs correct array of hashes' do
+    data = [
+      ["first_name", "last_name", "city", "state"],
+      ["Elisabeth", "Gardenar", "Toledo", "OH"],
+      ["Jamaal", "Du", "Sylvania", "OH"],
+      ["Kathlyn", "Lavoie", "Maumee", "OH"]
+    ]
+
+    converted_table = [
+     { "first_name" => "Elisabeth", "last_name" => "Gardenar", "city" => "Toledo", "state" => "OH" },
+     { "first_name" => "Jamaal", "last_name" => "Du", "city" => "Sylvania", "state" => "OH" },
+     { "first_name" => "Kathlyn", "last_name" => "Lavoie", "city" => "Maumee", "state" => "OH" }
+    ]
+    expect(convert_table(data)).to eq converted_table
+  end
+
 end
