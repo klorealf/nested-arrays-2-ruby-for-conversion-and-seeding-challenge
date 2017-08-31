90e6db8f44dbeafcb9a3c286499dc20d03732007
David Skaggs <davidtskaggs@gmail.com>
2017-08-31 18:54:43 -0400

    Add rspec tests

--- spec/tic_tac_toe_spec.rb
+++ spec/tic_tac_toe_spec.rb
@@ -1,4 +1,12 @@
 require_relative '../tic_tac_toe'
 
 describe 'generating a tic-tac-toe board' do
+  it "is returns an array" do
+    expect(generate_tic_tac_toe.class).to eq Array
+  end
+
+  it "it has a length of ..." do
+    game_board_length = generate_tic_tac_toe.length
+    expect(game_board_length).to eq 3
+  end
 end
