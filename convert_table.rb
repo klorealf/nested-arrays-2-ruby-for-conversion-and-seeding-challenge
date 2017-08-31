8462dadb783e1b797b46bfdd49e372550731a41c
Andreas Bach Landgrebe <andreaslandgrebe@gmail.com>
2017-08-31 17:11:46 -0400

    complete release 2 by converting a nested array of table data to a hash

--- convert_table.rb
+++ convert_table.rb
@@ -1,5 +1,10 @@
 
-def convert_table(table_data)
-  table_data = []
-  hash_return = {}
+
+def convert_table(table)
+
+  table[1..-1].map{|row| Hash[table[0].zip(row)]}
 end
+
+#p convert_table(table_data)
+
+
--- spec/convert_table_spec.rb
+++ spec/convert_table_spec.rb
@@ -1,4 +1,24 @@
 require_relative '../convert_table'
 
 describe 'converting a roster from a nested array to a collection of hashes' do
+  #let(:table_data) { convert_table }
+  #table_data = [
+  #["first_name", "last_name", "city", "state"],
+  #["Elisabeth", "Gardenar", "Toledo", "OH"],
+  #["Jamaal", "Du", "Sylvania", "OH"],
+  #["Kathlyn", "Lavoie", "Maumee", "OH"]
+  table_data = [
+  ["first_name", "last_name", "city", "state"],
+  ["Elisabeth", "Gardenar", "Toledo", "OH"],
+  ["Jamaal", "Du", "Sylvania", "OH"],
+  ["Kathlyn", "Lavoie", "Maumee", "OH"]
+]
+
+  it "display nested array of table data to hash" do
+   expect(convert_table(table_data)).to eq [
+  { "first_name" => "Elisabeth", "last_name" => "Gardenar", "city" => "Toledo", "state" => "OH" },
+  { "first_name" => "Jamaal", "last_name" => "Du", "city" => "Sylvania", "state" => "OH" },
+  { "first_name" => "Kathlyn", "last_name" => "Lavoie", "city" => "Maumee", "state" => "OH" }
+]
+  end
 end
