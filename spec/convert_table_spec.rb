400687b106b99c90d9af8f9553ebc4c572257c51
David Skaggs <davidtskaggs@gmail.com>
2017-08-31 18:41:26 -0400

    Add additional tests

--- spec/convert_table_spec.rb
+++ spec/convert_table_spec.rb
@@ -8,6 +8,11 @@ describe 'converting a roster from a nested array to a collection of hashes' do
   ["Kathlyn", "Lavoie", "Maumee", "OH"]
 ]}
 
+it "is an array of hashes" do
+    data_str_class = convert_table(data).class
+    expect(data_str_class).to eq Array
+  end
+
   it "converts a nested array to a nested array of hashes" do
     convert_data_str = convert_table(data)
     expect(convert_data_str).to eq [{ "first_name" => "Elisabeth", "last_name" => "Gardenar", "city" => "Toledo", "state" => "OH" },
@@ -15,8 +20,8 @@ describe 'converting a roster from a nested array to a collection of hashes' do
                                     { "first_name" => "Kathlyn", "last_name" => "Lavoie", "city" => "Maumee", "state" => "OH" }]
   end
 
-  it "converts specific keys" do
-    convert_data_str = convert_table(data)
-    expect(convert_data_str[0][0]).to eq  "first_name"
+  it "is has a length of 3" do
+    data_str_class = convert_table(data).length
+    expect(data_str_class).to eq 3
   end
 end
