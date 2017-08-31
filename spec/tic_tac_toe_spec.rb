80308b1ad19a8e733f347467142d4f4e4be07e27
klorealf <email": null>
2017-08-31 16:45:26 -0400

    add test for rows

--- spec/tic_tac_toe_spec.rb
+++ spec/tic_tac_toe_spec.rb
@@ -1,4 +1,25 @@
 require_relative '../tic_tac_toe'
 
+#it returns a board populated with X's and O's, but we don't know their exact order. So, we can't test for equality. We can't say that our method will return an exact value.
+
+
 describe 'generating a tic-tac-toe board' do
+  let(:board) {generate_tic_tac_toe}
+
+  it 'has three rows' do
+    row = board.count
+    expect(row).to eq(3)
+  end
+
+  it 'has three columns' do
+
+  end
+
+  it 'each letter on the board is either an X or an O' do
+
+  end
+
+  it 'returns a board randomly populated with Xs and 0s' do
+  end
+
 end
