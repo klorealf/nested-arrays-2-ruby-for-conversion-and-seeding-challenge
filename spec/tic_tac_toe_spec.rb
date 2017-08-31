74be7b37126c990cecd3797fb0228d8b08dcf4fd
Chris Bordes <cbordes8@gmail.com>
2017-08-31 12:04:01 -0400

    Add code for Release 1

--- spec/tic_tac_toe_spec.rb
+++ spec/tic_tac_toe_spec.rb
@@ -1,4 +1,28 @@
 require_relative '../tic_tac_toe'
 
 describe 'generating a tic-tac-toe board' do
+  it "contains 3 inside of nested array" do
+    expect(generate_tic_tac_toe.length).to eq 3
+  end
+
+describe 'generating a tic-tac-toe board' do
+  it 'contains three columns' do
+    expect(generate_tic_tac_toe.all? { |index| index.length}).to eq true
+  end
+
+describe 'generating a tic-tac-toe board' do
+  it "contains either X's or O's" do
+  generate_tic_tac_toe.each do |index|
+    index.each do |letters|
+      expect(letters).to eq("X").or(eq("O"))
+       end
+     end
+    end
+  end
 end
+
+end
+
+
+
+
--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -1,5 +1,4 @@
 def generate_tic_tac_toe
-
-
-
+pieces = "X", "O"
+Array.new(3) {Array.new(3) {pieces[rand(2)]} }
 end
