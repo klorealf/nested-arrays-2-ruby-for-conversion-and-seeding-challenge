cde76c80a6865e2d5ac707c860fb124de6977c67
Catherine Rice <clr2107@gmail.com>
2017-08-31 11:20:05 -0400

    Update test

--- spec/tic_tac_toe_spec.rb
+++ spec/tic_tac_toe_spec.rb
@@ -3,15 +3,15 @@ require_relative '../tic_tac_toe'
 describe 'generating a tic-tac-toe board' do
 
   it 'has three rows' do
-
+    expect(generate_tic_tac_toe[0][0].length + generate_tic_tac_toe[1][0].length + generate_tic_tac_toe[2][0].length).to eq (3)
   end
 
   it 'has three columns' do
     expect(generate_tic_tac_toe[0].length).to eq (3)
   end
 
-  it 'each letter on the board is either an X or an O' do
-    expect(generate_tic_tac_toe[0].sample).to eq (("X") || ("O"))
+  it 'each letter on the board is either an X or an O'
+    expect(generate_tic_tac_toe[0].sample).to include("X", "O")
   end
 
 end
